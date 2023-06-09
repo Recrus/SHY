c5cdd250e676e4c1aab2a2313433899c363ea96b
Recrus <dan.plishkin@gmail.com>
2023-06-10 00:17:03 +0300

    feat: signup with dark mode and 404 pages

--- frontend/src/views/Errors/NoContent.jsx
+++ frontend/src/views/Errors/NoContent.jsx
@@ -7,7 +7,7 @@ function NoContent() {
         <div className='bg-gradient-to-r from-purple from-10% via-primary via-30% to-pink to-90% h-screen flex justify-center items-center flex-col'>
             <div className='mb-4 text-[54px] text-neutral'>404</div>
             <div className='mb-4 text-sm text-neutral'>Hmm, looks like this page doesn't exist.</div>
-            <div className='drop-shadow-lg p-4 bg-neutral rounded'>
+            <div className='drop-shadow-lg p-4 bg-neutral rounded transition ease-in-out delay-150 hover:bg-accent hover:text-neutral'>
                 <Link to='/landing'>Go home</Link>
             </div>
         </div>
